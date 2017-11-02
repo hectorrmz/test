8be50431653c387d12b32b2b93bee20802e81f03
Hector Ramirez <hector.rmz11@gmail.com>
2017-11-01 23:20:41 -0600

    added calendar view, added hours summary

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,5 +1,6 @@
 import { Inject } from '../decorators/decorators';
 import { IAuthService } from '../services/auth.service';
+import * as moment from 'moment';
 
 declare interface weekDay {
     name: string; // S-M-T-W-T-F-S
@@ -11,7 +12,7 @@ class weekObject {
     weekDays: Array<weekDay> = [];
 
     constructor() {
-        var days = ["Sunday", "Monday", "Tuesday", "Wendesday", "Thursday", "Friday", "Saturday"];
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
         days.forEach((dayName: string, index: number) => {
             this.weekDays.push({ name: dayName, day: index });
@@ -30,6 +31,7 @@ export class HomeController {
     public events: Array<any> = [];
 
     public weeks: Array<weekObject> = [];
+    public entries: Array<any> = [];
 
 
     constructor(private _authService: IAuthService, private _authHelper: IAuthHelper) {
@@ -40,64 +42,33 @@ export class HomeController {
     private setdaysRange() {
         var now = new Date();
 
-        if (now.getDate() <= 15) {
+        var firstDay: number = now.getDate() <= 15 ? 1 : 16;
+        var end: number = now.getDate() <= 15 ? 15 : this.daysInMonth(now.getMonth(), now.getFullYear());
 
-            var initial = new Date(`${now.getMonth() + 1}-1-${now.getFullYear()}`);
-            var end: number = 15;
-            console.log(initial);
+        var initial = new Date(`${now.getMonth() + 1}-${firstDay}-${now.getFullYear()}`);
+        //console.log(initial);
 
-            var initialNumber: number = initial.getDay(); // 0-6
-            var dayNumber = initial.getDate(); // date 1
-            var skip = false;
-            while (end > dayNumber) {
-                var weekObj = new weekObject();
+        var initialNumber: number = initial.getDay(); // 0-6
+        var dayNumber = initial.getDate(); // date 1
+        var skip = false;
+        while (end > dayNumber) {
+            var weekObj = new weekObject();
 
-                weekObj.weekDays.forEach((day, index) => {
+            weekObj.weekDays.forEach((day, index) => {
 
-                    if ((index >= initialNumber || skip) && dayNumber <= end) {
-                        day.date = dayNumber;
-                        dayNumber++;
-                    }
-                    console.log(day, index);
-                });
+                if ((index >= initialNumber || skip) && dayNumber <= end) {
+                    day.date = dayNumber;
+                    dayNumber++;
+                }
+                //console.log(day, index);
+            });
 
-                skip = true;
+            skip = true;
 
-                this.weeks.push(weekObj);
-            }
-
-            console.log(this.weeks);
-
-
-        } else {
-
-            var initial = new Date(`${now.getMonth() + 1}-16-${now.getFullYear()}`);
-            var end = 31;
-            console.log(initial);
-
-            var initialNumber: number = initial.getDay(); // 0-6
-            var dayNumber = initial.getDate(); // date 16-31
-            var skip = false;
-            while (end > dayNumber) {
-                var weekObj = new weekObject();
-
-
-                weekObj.weekDays.forEach((day, index) => {
-
-                    if ((index >= initialNumber || skip) && dayNumber <= end) {
-                        day.date = dayNumber;
-                        dayNumber++;
-                    }
-                    console.log(day, index);
-                });
-
-                skip = true;
-
-                this.weeks.push(weekObj);
-            }
-
-            console.log(this.weeks);
+            this.weeks.push(weekObj);
         }
+
+        console.log(this.weeks);
     }
 
     public login = (form: ng.IFormController) => {
@@ -112,7 +83,7 @@ export class HomeController {
             };
 
             this._authService.loginRM(user).then((response: any) => {
-                console.log(response.data.user);
+                //console.log(response.data.user);
 
                 this.rdUser = response.data.user;
                 this._authHelper.AuthorizeUser(this.rdUser);
@@ -120,7 +91,7 @@ export class HomeController {
                 this.getIssues();
 
             }, (response) => {
-                console.log(response);
+                //console.log(response);
                 this.errorMsg = response.data.message;
             });
         }
@@ -148,14 +119,33 @@ export class HomeController {
 
     private getTimes = (key: string, id: number, issueId: number) => {
 
-        let dateRange = "><2017-10-01|2017-10-31";
+        let dateRange = "><2017-11-01|2017-11-30";
 
         this._authService.getTimeEntries(key, id, issueId, dateRange).then((rsp: any) => {
             this.timeEntries = rsp.data.time_entries;
-            console.log(this.timeEntries);
+            //console.log(this.timeEntries);
             this.renderRedMineEvents();
+            this.createTimeEntries();
         });
 
+
+    }
+
+    private createTimeEntries() {
+
+        this.timeEntries.forEach((time: ITimeEntry) => {
+
+            var date = moment(time.spent_on).date();
+
+            var entry = {
+                title: time.comments,
+                duration: time.hours,
+                date: date,
+                activity: time.activity
+            }
+
+            this.entries.push(entry);
+        });
 
     }
 
@@ -192,7 +182,7 @@ export class HomeController {
 
             event.color = this.setEventColor(time.activity.id);
 
-            console.log(event);
+            //console.log(event);
 
             this.events.push(event);
         });
@@ -201,6 +191,20 @@ export class HomeController {
 
 
     };
+
+    public getTotal(date: number): number {
+        var total: number = 0;
+
+        var entries: Array<any> = this.entries.filter((entry: any) => {
+            return entry.date == date;
+        });
+
+        entries.forEach((entry: any) => {
+            total += entry.duration;
+        });
+
+        return total;
+    }
 
     private setEventColor = (activity: number) => {
         switch (activity) {
@@ -212,6 +216,10 @@ export class HomeController {
             default: return "#ffd480";
         }
     };
+
+    private daysInMonth(month: number, year: number) {
+        return 32 - new Date(year, month, 32).getDate();
+    }
 
     /** Initializes the controller. */
     $onInit(): void { }
